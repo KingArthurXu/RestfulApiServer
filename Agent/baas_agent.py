fa745ca9b83a10796142b84613951334303798b0
ArthurXu <3540710@qq.com>
2019-03-11 08:52:29 +0800

    add baas_agent.py

---
+++ Agent/baas_agent.py
@@ -0,0 +1,19 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：      baas_agent
+   Description:
+   Author:          'Arthur Xu'
+   date:            2019/3/11
+-------------------------------------------------
+   Change Activity:
+   2019/3/11:     
+-------------------------------------------------
+"""
+__author__ = 'Arthur Xu'
+
+import nb_agent
+nb_agent.start()
+
+
